ed731d2688ef222e23df06187bb1c26f2d992c08
Javier Cabezas <jcleyton@gmail.com>
2017-02-13 22:34:14 -0300

    Algorithms:circular-array-rotation Optimizations
    Its not really needed to do many rotations when you can actually just
    do one.

--- algorithms/circular-array-rotation/index.php
+++ algorithms/circular-array-rotation/index.php
@@ -13,19 +13,21 @@ $a_temp = fgets($handle);
 $a = explode(" ",$a_temp);
 array_walk($a,'intval');
 
+//Optimization: Don't do any unnecesary rotations
+$k = $k % $n;
+
 //Rotate the array
-for($i = 0 ; $i < $k ; $i++){
-    //We make a copy of a on a temporal array
-    $clone = $a;
-    $a = [];
-    //Then we fill the new a array with the value of each element in the key +1 modulo array lenght.
-    //By using this algorithm the last value will go to the first position (by having 0 as an index) and everything else will go foward 1 position.
-    foreach($clone as $key => $value){
-        $a[ ($key + 1)% $n ] = $value;
-    }
-    //Finally we call ksort so they array is sorted by its keys, leaving the element with 0 as the index as the first one.
-    ksort($a);
+
+//We make a copy of a on a temporal array
+$clone = $a;
+$a = [];
+
+//Then we fill the new a array with the value of each element in the key +1 modulo array lenght.
+//By using this algorithm the last value will go to the first position (by having 0 as an index) and everything else will go foward 1 position.
+foreach($clone as $key => $value){
+    $a[ ($key + $k)% $n ] = $value;
 }
+
 
 for($a0 = 0; $a0 < $q; $a0++){
     fscanf($handle,"%d",$m);
@@ -37,5 +39,3 @@ for($a0 = 0; $a0 < $q; $a0++){
 foreach($out as $temp){
     echo $temp."\n";
 }
-
-
