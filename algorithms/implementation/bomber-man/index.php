679d730876feaf67ae9163dc1fad5dae82a9a473
Javier Cabezas <jcleyton@gmail.com>
2017-02-18 22:13:42 -0300

    Algorithms:bomber-man read input data

--- algorithms/implementation/bomber-man/index.php
+++ algorithms/implementation/bomber-man/index.php
@@ -7,3 +7,37 @@
  * Time: 7:05 PM
  */
 $_fp = fopen("php://stdin", "r");
+
+$handle = fopen ("php://stdin","r");
+fscanf($handle,"%d %d %d",$r,$c,$n);
+
+$matrix = [];
+for($row = 0; $row < $r ; $row += 1){ //Read all the rows
+    $complete_row = fgets($handle);
+    $matrix[$row] = [];
+    for($column = 0; $column < strlen($complete_row) -1 ; $column += 1) { //The -1 is because we dont want the \n character
+        $matrix[$row][$column] = $complete_row[$column];
+    }
+}
+
+/**
+ * Prints the matrix on the CLI.
+ * @param $matrix
+ * Ex:
+ *      .......
+ *      ...O...
+ *      ....O..
+ *      .......
+ *      OO.....
+ *      OO.....
+ */
+function print_matrix($matrix){
+    foreach($matrix as $row){
+        foreach($row as $data){
+            echo $data;
+        }
+        echo "\n";
+    }
+}
+
+echo print_matrix($matrix);
