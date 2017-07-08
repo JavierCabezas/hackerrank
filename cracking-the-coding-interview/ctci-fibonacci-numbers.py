71e38311ee7cce43aac0891738a6a113535e2d9c
Javier Cabezas <jcleyton@gmail.com>
2017-07-08 19:13:55 -0400

    Cracking the code interview: Recursive fibonacci with memory to improve speed

--- cracking-the-coding-interview/ctci-fibonacci-numbers.py
+++ cracking-the-coding-interview/ctci-fibonacci-numbers.py
@@ -1,8 +1,13 @@
+values = {}
 def fibonacci(n):
-    if n > 1:
-        return fibonacci(n-1) + fibonacci(n-2)
+    if n in values:
+        return values[n]
     else:
-        return n
+        if n > 1:
+            values[n] = fibonacci(n-1) + fibonacci(n-2)
+            return values[n]
+        else:
+            return n
 
 n = int(input())
 print(fibonacci(n))
