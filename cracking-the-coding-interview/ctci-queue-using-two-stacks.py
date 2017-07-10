3e82eb7a1760fe66d56222e496256df971423295
Javier Cabezas <jcleyton@gmail.com>
2017-07-09 22:40:17 -0400

    Cracking the code interview: Queue using two stacks solution 1

---
+++ cracking-the-coding-interview/ctci-queue-using-two-stacks.py
@@ -0,0 +1,27 @@
+class MyQueue(object):
+    def __init__(self):
+        self.list = []
+    def peek(self):
+        if len(self.list) > 0:
+            return self.list[0]
+        else:
+            return None
+    def pop(self):
+        if len(self.list) > 0:
+            del self.list[0]
+    def put(self, value):
+        self.list.append(value)
+
+queue = MyQueue()
+t = int(input())
+for line in range(t):
+    values = map(int, input().split())
+    values = list(values)
+    if values[0] == 1:
+        queue.put(values[1])
+    elif values[0] == 2:
+        queue.pop()
+    else:
+        print(queue.peek())
+
+
