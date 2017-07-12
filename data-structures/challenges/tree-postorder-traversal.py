b51d2185dfd9534938c93a478b52d99081759eab
Javier Cabezas <jcleyton@gmail.com>
2017-07-11 23:31:57 -0400

    Data Structures: Tree postorder traversal

---
+++ data-structures/challenges/tree-postorder-traversal.py
@@ -0,0 +1,12 @@
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def postOrder(root):
+    if root.left is not None:
+        postOrder(root.left)
+    if root.right is not None:
+        postOrder(root.right)
+    print str(root.data),
