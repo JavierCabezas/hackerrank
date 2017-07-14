4058ef9eed1d5fcaa7d4a0cb54ca30b9f4624c5b
Javier Cabezas <jcleyton@gmail.com>
2017-07-13 21:26:08 -0400

    Python: Symetric difference solution 2

--- python/challenges/symmetric-difference.py
+++ python/challenges/symmetric-difference.py
@@ -1,15 +1,10 @@
-def discard_all(parent_set, set_to_discard):
-    for i in set_to_discard:
-        parent_set.discard(i)
-    return parent_set
-
 n = int(input())
 set_1 = set(int(x) for x in input().split(' '))
 m = int(input())
 set_2 = set(int(x) for x in input().split(' '))
 
 inter = set_1.intersection(set_2)
-result = discard_all(set_1, inter).union(discard_all(set_2, inter))
+result = set_1.difference(inter).union(set_2.difference(inter))
 
 for x in sorted(result):
     print(x)
