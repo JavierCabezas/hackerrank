44903e0c142f87133a10d8f52b7e7f3ba4fbb115
Javier Cabezas <jcleyton@gmail.com>
2017-07-27 23:14:13 -0400

    Cracking the code interview bubble sort solution

---
+++ cracking-the-coding-interview/ctci-bubble-sort.py
@@ -0,0 +1,13 @@
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
+
+number_swaps = 0
+for i in range(n):
+    for j in range(n - 1):
+        if a[j+1] < a[j]:
+            a[j + 1], a[j] = a[j], a[j + 1]
+            number_swaps += 1
+
+print("Array is sorted in {0} swaps.".format(number_swaps))
+print("First Element: {0}".format(a[0]))
+print("Last Element: {0}".format(a[-1]))
