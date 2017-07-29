a37bda66413891430a3ebfea01f504a5190be82b
Javier Cabezas <jcleyton@gmail.com>
2017-07-29 15:50:16 -0400

    Cracking the coding interview: Lonely integer smarter solution

--- cracking-the-coding-interview/ctci-lonely-integer.py
+++ cracking-the-coding-interview/ctci-lonely-integer.py
@@ -1,21 +1,12 @@
 def lonely_integer(a):
     if len(a) < 1:
         return False
-    if len(a) < 2:
-        return a[0]
 
-    a.sort()
-    last_number = None
-    for i in range(len(a)):
-        if last_number is not None:
-            if last_number != a[i]:
-                return last_number # We found the lonely integer
-            else:
-                last_number = None #Ok, they are equal. Reset the array
-        else:
-            last_number = a[i]
+    out = a[0]
+    for i in range(1, len(a)):
+        out = out ^ a[i]
+    return out
 
-    return last_number #If we didn't return anything so far then the last_number stored is the lonely integer.
 
 n = int(input().strip())
 a = [int(a_temp) for a_temp in input().strip().split(' ')]
