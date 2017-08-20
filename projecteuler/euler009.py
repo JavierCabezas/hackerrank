8a2450d4ba3cd77fa039d6696e7d648f022fec11
Javier Cabezas <jcleyton@gmail.com>
2017-08-20 16:42:44 -0300

    Project Euler #9: Special Pythagorean triplet solution

--- projecteuler/euler009.py
+++ projecteuler/euler009.py
@@ -1,11 +1,23 @@
-from functools import reduce
-
 number_of_testcases = int(input())
 numbers_to_check = [int(input()) for _ in range(number_of_testcases)]
 
+#From a+b+c = n
+#And a2 + b2 = c2
+#We can get b = (n2 - 2an) / (2n-2a)
+#n is known (in array numbers_to_check), so we must loop just for values of "a".
+
+def get_b(a, n):
+    return (n**2 - 2*a*n) / (2*(n-a))
+
 for n in numbers_to_check:
-    pairs = [[a, b, c] for a in range(1, n-2) for b in range(1, n-2) for c in range(1, n-2) if a**2 + b**2 == c**2]
-    if len(pairs) == 0:
+    max_b = 0
+    max_a = 0
+    for a in range(1, n-1):
+        b = get_b(a, n)
+        if b.is_integer() and b*a > max_b*max_a:
+            max_b = b
+            max_a = a
+    if max_b == 0:
         print(-1)
     else:
-        print(reduce(lambda x, y: x*y, pairs[-1]))
+        print (int(max_a*max_b*(n-max_b-max_a)))
