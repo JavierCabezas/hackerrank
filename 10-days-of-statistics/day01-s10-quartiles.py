6429580858d1508e1542243819fbbdb581cf3791
Javier Cabezas <jcleyton@gmail.com>
2017-08-24 22:52:48 -0300

    Day 1: Quartiles solution

---
+++ 10-days-of-statistics/day01-s10-quartiles.py
@@ -0,0 +1,24 @@
+i_wont_use_you = int(input())
+numbers = list(map(int, input().split(" ")))
+numbers.sort()
+
+def get_average(arr):
+    return sum(arr) / len(arr)
+
+def get_mean(arr):
+    n = len(arr)
+    if n % 2 == 0:
+        return get_average([arr[n//2], arr[(n//2)-1]])
+    else:
+        return arr[n//2]
+
+first_half = numbers[:len(numbers)//2]
+second_half = numbers[-(len(numbers)//2):]
+
+q1 = get_mean(first_half)
+q2 = get_mean(numbers)
+q3 = get_mean(second_half)
+
+print("%d" % q1)
+print("%d" % q2)
+print("%d" % q3)
