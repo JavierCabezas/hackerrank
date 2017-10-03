c8a2db240498149f7a756c135a69f7b58c89290d
Javier Cabezas <jcleyton@gmail.com>
2017-10-02 22:47:17 -0300

    Algorithms The Grid Search solution =D

--- algorithms/implementation/the-grid-search.py
+++ algorithms/implementation/the-grid-search.py
@@ -1,37 +1,24 @@
-def is_word_in_row(word, row, col):
-    '''
-    This method checks if the word given by params is on the row
-    :param word: word to find
-    :param row: row matrix in where we are going to look for the word
-    :param col: The column in where the last word was found. In case its given then the next word must be on the same column, otherwise we didn't find the word.
-    In case its -1 it means that we don't know the column value yet so we are setting a new one.
-    :return:
-    '''
-    try:
-        col_pos = row.index(word)
-        if col is -1:
-            return col_pos #Set new column value since we didn't had an old one.
-        else:
-            if col_pos == col:
-                return col_pos #Everything its OK. The word was found and it starts on the same column.
-            else:
-                return -1 #Word found but not on the same column
-    except ValueError:
-        return -1
+def check_if_all_are_equal(matrix, to_find, n_row, n_col):
+    rows_to_find = len(to_find)
+    cols_to_find = len(to_find[0])
 
+    for row_offset in range(rows_to_find):
+        for col_offset in range(cols_to_find):
+            if matrix[n_row + row_offset][n_col + col_offset] != to_find[row_offset][col_offset]:
+                return False
+    return True
 
-def check_matrix(big_matrix, to_find):
-    for row_idx_to_check in range(len(big_matrix) - len(to_find)):
-        idx_to_find = 0
-        found_in_what_col = -1
-        for idx_offset in range(len(to_find)):
-            found_in_what_col = is_word_in_row(to_find[idx_to_find], big_matrix[row_idx_to_check + idx_to_find], found_in_what_col)
-            if found_in_what_col > 0:
-                idx_to_find += 1
-            if len(to_find) == idx_to_find:
-                return True
+def check_matrix(matrix, to_find):
+    n_rows = len(matrix) - len(to_find) + 1
+    n_cols = len(matrix[0]) - len(to_find[0]) + 1
 
-    return False  # Saw all the elements in the matrix without success
+    for n_row in range(n_rows):
+        for n_col in range(n_cols):
+            if matrix[n_row][n_col] == to_find[0][0]:
+                if check_if_all_are_equal(matrix, to_find, n_row, n_col):
+                    return True
+
+    return False
 
 
 number_of_testcases = int(input())
